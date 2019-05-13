b0e7f7e6e13492caad6de05704668f8d5220d89e
HeOfLittleSleep <pottim99@gmail.com>
2019-05-12 23:35:57 -0500

    skdjflskdfgsdfg

--- dates.js
+++ dates.js
@@ -14,7 +14,8 @@
       dd days, hh hrs, mm mins, ss secs
 */
 
-function showDateTime(time) {
+function showDateTime(time) 
+{
    date = time.getDate();
    month = time.getMonth()+1;
    year = time.getFullYear();
@@ -33,4 +34,27 @@ function showDateTime(time) {
    return month+"/"+date +"/"+year+" at "+hour+":"+minute+":"+second+ampm;
 }
 
-function 
+function countdown(stop, start)
+{
+	var MonthDays = new Array("31", "29", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31");
+	
+	monthsLeft = stop[0] - start[0];
+	
+	//daysLeft = (stop[1] - start[1] < 0) ? (stop[1] - start[1]) + MonthDays[stop[0] - 1] : stop[1] - start[1];
+	if (stop[1] - start[1] < 0)
+	{
+		currentMonth = stop[0];
+		daysLeft = (stop[1] - start[1]) - (0 - MonthDays[currentMonth - 1]);   //don't look. I am ashamed. (it was concatenating instead of adding)
+		monthsLeft = monthsLeft - 1;
+	}
+	else
+	{
+		daysLeft = stop[1] - start[1];
+	}
+	
+	hoursLeft = stop[2] - start[2];
+	minutesLeft = stop[3] - start[3];
+	secondsLeft = 60 - start[4];
+	
+	return monthsLeft+" months "+daysLeft +" days "+hoursLeft+" hours "+minutesLeft+" minutes and "+secondsLeft+" seconds";
+}
